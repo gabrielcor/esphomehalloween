c6e8d308050fdade29ff4922bf2a661c62c93cdd
Gabriel Cor <gabrielcor@hotmail.com>
2025-11-01 12:34:04 -0300

    volver a configuración de humo apagado y tb agregar compilado en comando de upload

--- uploadseed.cmd
+++ uploadseed.cmd
@@ -1,2 +1,4 @@
+esphome compile seed-relay-01.yaml
+esphome compile seed-relay-02.yaml
 esphome upload --device 192.168.50.244 seed-relay-01.yaml
 esphome upload --device 192.168.50.82 seed-relay-02.yaml
